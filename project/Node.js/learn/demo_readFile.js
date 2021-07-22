d21228fff052e6637bcfc041dacb2c9968d9a614
tsonkqolz <13umvursukuf@gmail.com>
2021-07-22 19:56:20 +0700

    gaym

--- project/Node.js/learn/demo_readFile.js
+++ project/Node.js/learn/demo_readFile.js
@@ -2,9 +2,9 @@ import http from 'http';
 import fs from 'fs';
 
 http.createServer((req, res) => {
-    fs.readFile('./demofile1.html', (err, data) => {
+    fs.readFile('demofile1.html', (err, data) => {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(typeof data)
-        res.end();
+        res.write(data);
+        return res.end();
     })
-})
+}).listen(8080)
