257027a8bb00abfe5f7fa2af9a58efc612d6d503
tsonkqolz <13umvursukuf@gmail.com>
2021-07-23 11:29:06 +0700

    boom

--- project/Node.js/learn/demo_querystring.js
+++ project/Node.js/learn/demo_querystring.js
@@ -1,10 +1,9 @@
 import http from 'http';
-import url from 'url';
 
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'})
-    let q = url.parse(req.url, true).query
+    let q = new URL(req.headers.host + req.url).searchParams
     // query type is object
-    res.write(JSON.stringify(q))
+    res.write(q.toString())
     res.end()
 }).listen(8080)
