efaeaff56196b1ecb50e106d9989b7dd257e3ab5
tsonkqolz <13umvursukuf@gmail.com>
2021-07-25 20:58:05 +0700

    sus

--- project/Node.js/express/express-locallibrary-tutorial/routes/users.js
+++ project/Node.js/express/express-locallibrary-tutorial/routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/lol', function(req, res, next) {
+  res.send('nice')
+})
+
 module.exports = router;
